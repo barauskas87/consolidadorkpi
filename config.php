c7435c339d2d701af40ae30ffb73af7435e346fc
barauskas87 <56981080+barauskas87@users.noreply.github.com>
2025-07-28 16:43:41 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 // Configurações do banco e token de autenticação
 
-// Dados do banco MySQL
+// Dados do banco Azure SQL
 define('DB_HOST', 'ds-srv01.database.windows.net');
-define('DB_USER', 'masterdsadm');
-define('DB_PASS', 'Ms!D@tAS1tE2o#4');
-define('DB_NAME', 'CONSOLIDADOR_KPI');
+define('DB_USER', 'datasite_user');
+define('DB_PASS', 'Dat@_$1te854p44%!');
+define('DB_NAME', 'DatasitePRD');
 
 // Token fixo para autenticação da API
 // Altere para um valor seguro e secreto
